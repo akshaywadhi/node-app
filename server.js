7e72d5e19aa9db1ed80f10cc504f765481c08ea3
akshaywadhi <akshaywadhi.cs@gmail.com>
2024-12-23 12:12:18 +0530

    changes

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.json())
 app.use(cors())
 app.use('/users', router.router)
 app.use(express.static(path.resolve(__dirname , 'public/build')))
+app.use('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 
 
